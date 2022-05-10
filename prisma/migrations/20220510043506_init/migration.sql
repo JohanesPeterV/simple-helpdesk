9911635ebde1945adcc8bed3899c80fdf59c13d7
JopHme <60497959+JopHme@users.noreply.github.com>
2022-05-10 12:04:28 +0700

    refactor: refactor ticket history naming conventions

--- prisma/migrations/20220510043506_init/migration.sql
+++ prisma/migrations/20220510043506_init/migration.sql
@@ -9,6 +9,7 @@ CREATE TABLE "Admin" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "resignedAt" TIMESTAMP(3),
 
     CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
 );
